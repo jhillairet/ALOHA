1448a5ecc32c7585b0022d39dac58918be431014
Julien Hillairet <julien.hillairet@cea.fr>
2010-02-25 12:29:29 +0000

    Modif on aloha on demand script

--- on_demand/runaloha.m
+++ on_demand/runaloha.m
@@ -1,5 +1,5 @@
 function runaloha(pulsenb, t_start, t_end, varargin)
-% ALOHA ON DEMAND
+% ALOHA ON-DEMAND
 % 
 % This script run the LH coupling code ALOHA for a specific TS pulse
 % 
@@ -27,62 +27,11 @@ function runaloha(pulsenb, t_start, t_end, varargin)
 % LAST UPDATE:
 %  - 09/10/2009 : creation
 
-% In order to run ALOHA, we need an input scenario
-% This input scenario set up all the options, such
-% as the precision dnz of the spectrum.
-% 
-% We use the defaut ALOHA scenario example and we adapt it after
-actual_dir=pwd;
-cd('../scenario');
-sc=scenario_example;
-cd(actual_dir);
-
-
-% Ask for the antenna port to choose for excitation
-if not(exist('TSport'))
-    TSport=upper(input('Which port  : ''Q6A'' [C2 before 2009, C3 after] or  ''Q6B'' [C3 before 2009, C4 after] ? : '));
-end
-% check user input
-if not(strcmp('Q6A', TSport)) && not(strcmp('Q6B', TSport))
-    error('Bad port input : should be either Q6A or Q6B only.');
-end
-
-% Adaptation of the scenario 
-sc.options.TSport = TSport;   
-sc.options.choc = pulsenb;
-sc.options.tps_1 = t_start;
-sc.options.tps_2 = t_end;
-sc.options.bool_mesure = true; % activate the phase correction
-
-% the antenna type depends of the pulse number :
-% before #43540, C2 was on port Q6A and C3 on port Q6B
-% after  #43540, C3 was on port Q6A and C4 on port Q6B
-% #43540 was on end november 2008. 
-switch TSport
-    case 'Q6A'
-        if pulsenb < 43540
-            sc.antenna.architecture = 'antenne_C2';
-        else
-            sc.antenna.architecture = 'antenne_C3';
-        end
-    case 'Q6B'
-        if pulsenb < 43540
-            sc.antenna.architecture = 'antenne_C3';
-        else
-            sc.antenna.architecture = 'antenne_C4';
-        end         
-end
-
-% In any case, the frequency in the same : 3.7GHz;
-sc.antenna.freq = 3.7e9;
-
-% Now we get the feeding information from the TS database
-[sc.antenna.a_ampl, sc.antenna.a_phase] = aloha_antenna_excitation(pulsenb, t_start, t_end, TSport);
-
-% Asking for plasma properties such as electron density and gradient
+% Generate the input ALOHA scenario from TS database 
+sc = aloha_ondemand_scenario(pulsenb, t_start, t_end, varargin);
 
 % run ALOHA
-sc=aloha_scenario(sc);
+sc = aloha_scenario(sc);
 
 % save results into an ALOHA scenario file
 fileName = ['ALOHA_TS', num2str(pulsenb), '_', ...
@@ -91,11 +40,8 @@ fileName = ['ALOHA_TS', num2str(pulsenb), '_', ...
 aloha_scenario_save(sc, fileName);
 
 % convert and send ALOHA results into the TS database
-
-disp('Write into the Tore Supra database ?');
-pause
-
 % save results to be imported into the DB into a matlab binary file
+disp('Write into a file compatible to the Tore Supra database');
 aloha_ntnpar(sc); % TODO
 
 %  fileName = [specdat, int2str(pulsenb)];
