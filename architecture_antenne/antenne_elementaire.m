112b5c2beb9f36a66c752f20b20b9973a81998d1
Julien Hillairet <julien.hillairet@cea.fr>
2010-03-05 12:19:22 +0000

    Redefining an elementary antenna

--- architecture_antenne/antenne_elementaire.m
+++ architecture_antenne/antenne_elementaire.m
@@ -20,24 +20,24 @@ nb_g_passifs_bord = 0;		% nbre de guides passifs sur chaque bord
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % dimensions
 
-espacement_g_pol = 12e-3;	% espacement entre les grills juxtapos�s poloidalement 
+espacement_g_pol = 12e-3;	% espacement entre les grills juxtaposes poloidalement 
 
-a = 60e-3;			% hauteur des guides ds le sens poloidal  
+a = 70e-3;			% hauteur des guides ds le sens poloidal  
 
-antenne_standard = 1; % = 0 il faut d�crire l'antenne sur une ligne : tableaux b et e  
-                      % = 1 param�tres scalaires : b_g_actif, b_g_pass et e
+antenne_standard = 1; % = 0 il faut decrire l'antenne sur une ligne : tableaux b et e  
+                      % = 1 parametres scalaires : b_g_actif, b_g_pass et e
 
 
-b_g_actif = 5.5e-3;		% largeur des guides actifs 
-b_g_pass = 6.5e-3;		% largeur des guides passifs 
+b_g_actif = 8e-3;		% largeur des guides actifs 
+b_g_pass = NaN;		% largeur des guides passifs 
 
-e = 1.5e-3;			% �paisseur des parois des guides dans le sens toroidal 
+e = 2e-3;			% epaisseur des parois des guides dans le sens toroidal 
 
-lcc = 1/4;			% longueur du court-circuit (en lambda guid�e); 
+lcc = 1/4;			% longueur du court-circuit (en lambda guidee); 
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% matrice S des modules ds des fichiers .m (NB : la matrice est rang�e sur une seule colonne)
+% matrice S des modules ds des fichiers .m (NB : la matrice est rangee sur une seule colonne)
 
 % chemin_aller = '';
 % chemin_retour = '';
@@ -59,9 +59,9 @@ end
 
 % parametres 1D
 
-T_grill = 1;                % p�riodicit� du grill / diminue les tps de calcul
-D_guide_max = 100;          % d�couplage : pr |i-j| > D_guide_max, Kij = 0 avec {i,j} num�ros de deux guides => S  
+T_grill = 1;                % periodicite du grill / diminue les tps de calcul
+D_guide_max = 100;          % decouplage : pr |i-j| > D_guide_max, Kij = 0 avec {i,j} numeros de deux guides => S  
                             % puis pr |i-j| > D_guide_max -5, Sij = 0 	/ diminue encore les tps de calcul
 			    	  
-erreur_rel = 1e-6;  	    % erreur sur les int�grales
-pertes = 1e-6;              % tangente delta pour �viter les singularit�s	  
+erreur_rel = 1e-6;  	    % erreur sur les integrales
+pertes = 1e-6;              % tangente delta pour eviter les singularites	  
