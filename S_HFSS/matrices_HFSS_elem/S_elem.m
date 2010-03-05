d87c0b33f1d4d167d9d2bf524541e1788dbc42d2
Julien Hillairet <julien.hillairet@cea.fr>
2010-03-05 12:16:48 +0000

    add some comments into the file

--- S_HFSS/matrices_HFSS_elem/S_elem.m
+++ S_HFSS/matrices_HFSS_elem/S_elem.m
@@ -1,3 +1,9 @@
-S = [0,1;1,0];
+% ideal waveguide scattering matrix for one waveguide.
+% 
+% This matrix consist in the matrix of a 
+% perfect waveguide without transmission losses 
+% nor reflexion 
+S = [0,1; ...
+     1,0];
 
 S = reshape(S,1,4);
