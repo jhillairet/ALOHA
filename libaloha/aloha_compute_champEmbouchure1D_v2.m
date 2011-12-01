131346d398913e23062767d6632cc7cdda4fb363
Julien Hillairet <julien.hillairet@cea.fr>
2011-12-01 14:38:05 +0000

    Change the default dz value to 1e-6 in order to insure a correct sampling of the electric field calculation at the mouth. Some changes also in the structure of the code & comments.

--- libaloha/aloha_compute_champEmbouchure1D_v2.m
+++ libaloha/aloha_compute_champEmbouchure1D_v2.m
@@ -2,10 +2,28 @@ function scenario=aloha_compute_champEmbouchure1D_v2(scenario)
 % Compute the parallel component of electric field (Ez) in the  mouth of the antenna.
 % Calcule le champ electrique parallele (Ez) dans l'embouchure de l'antenne
 % 
+% scenario=aloha_compute_champEmbouchure1D_v2(scenario)
+% 
 % 1D case : the electric field correspond to the modes of a parallel-plate waveguide, ie 1 TEM and n TM modes.
+% However, the E & H fields amplitudes are normalized to the waveguide largest side (a), 
+% ie. the amplitudes would be similar in a full 3D modeling. 
+% 
+% If one wants to compare with a 2D modeling, he has to activate the 'SWAN' mode in ALOHA.
+% 
+% INPUT
+%   - scenario : ALOHA scenario (coupling allready calculated)
+% OUPUT
+%   - scenario : ALOHA scenario with the additionnal fields in the 'results' part:
+%           - abs_z(Nb_poloidal_lines, Nb_z)  : parallel abcisse z
+%           - Efield(Nb_poloidal_lines, Nb_z) : parallel electric field (Ez)
+%           - E_mouth(3, Nb_z, Nb_poloidal_lines) : Electric field [Ex,Ey,Ez]
+%           - H_mouth(3, Nb_z, Nb_poloidal_lines) : Magnetic field [Hx,Hy,Hz]
+%           - Poynting_mouth(3, Nb_z, Nb_poloidal_lines) : Poynting vector [Px,Py,Pz]
+%           - Poynting_mouth_total : total power flux integrated along the toroidal direction
 %  
-%  
-%  
+% AUTHOR: JH
+% LAST UPDATE:
+%  - 01/12/2012: corrected a bug in the z sampling, which was  
 
 % for easier matlab manipulation, load all the fields of the input scenario 'scenario'
 % into matlab workspace
@@ -19,14 +37,14 @@ k0 = 2*pi*freq/celerite;
 % test if the main results, such as the plasma scattering matrix, 
 % had been calculated. If not, warn user.
 if not(exist('S_plasma'))
-  error('The plasma scattering matrix has not been calculated !');
+  error('The plasma scattering matrix has not been calculated ! Please run aloha_scenario before.');
 end
 
 % Load into workspace the geometrical parameter of the scenario architecture 
 % and get the main geometrical parameters.
 if aloha_isAntennaITM(scenario)
-      disp(aloha_message('assuming ITM antenna description')); 
-      aloha_utils_ITM2oldAntenna; % convert ITM data to old fashioned ALOHA global parameters
+    disp(aloha_message('assuming ITM antenna description')); 
+    aloha_utils_ITM2oldAntenna; % convert ITM data to old fashioned ALOHA global parameters
     [b,h,z,y,nb_g_total_ligne,nbre_guides,act_module_tor]=aloha_utils_getAntennaCoordinatesFromCPO(aloha_getAntenna(scenario));
 else
     eval(architecture);
@@ -39,11 +57,30 @@ end
 % Lorsqu'on faisait la transformee de fourier du champ total, on obtenait alors un resultat "suprenant".
 % 
 % La modification du code consiste donc a exprimer les champs avec un pas constant et controle.
-dz = 1e-4; % ATTENTION : doit etre absolument inferieur a la plus petite decimale des dimensions b ou e
+% 
+% ATTENTION : doit etre absolument inferieur a la plus petite decimale des dimensions b ou e 
+% pour resoudre correctement spatialement en z.
+% Ex for the C4 launcher: dz=1e-6 since e=4.325mm
+dz = 1e-6; 
 
-% the ponderative factor are normalized to the waveguide largest side, 
-% in order to have the same unit than Efield calculated with the 2D ALOHA mode (or any 3D FEM code).
-% If one would compare this Efield with a 2D FEM code, then he has to remove the /a factor below...
+% The previous comment means that the total number of point should be an integer. 
+% ie: dz should be sufficiently small !
+%  number_z_points = (b(end)+z(end))/dz+1; 
+
+
+% Clear all output fields variables
+% PS: This is recommanded here since these variables can be overloaded from the 
+% input scenario.
+Efield  = [];
+E_mouth = [];
+H_mouth = [];
+Poynting_mouth = [];
+abs_z = [];
+
+% The E and H field amplitudes are normalized to the waveguide largest side. 
+% In order to have the same unit than Efield calculated with the 2D ALOHA mode (or any 3D FEM code).
+% If one would compare this Efield with a 2D FEM code, then he has to remove the /a factor below. 
+% This is automatically done when one activates the "SWAN" option in ALOHA.
 %  
 % Bug correction JH 13/11/2011
 % The magnetic field amplitude was lower than expected. This has an impact on
@@ -61,7 +98,9 @@ elseif type_swan_aloha == 0 % no "a" dependance (parallel-plate waveguide, 2D)
     poids_H = sqrt(2)*inv(rac_Zhe)*(a_plasma - b_plasma);
 end
 
-Efield=[];
+
+
+disp(aloha_message('Calculating Electric and Magnetic fields at the mouth of the launcher')); 
 % for all poloidal lines
 for idx_pol = 1:nb_g_pol 
     abs_z_idx_pol = [];
@@ -70,7 +109,7 @@ for idx_pol = 1:nb_g_pol
     % for all waveguides in a poloidal line
     for idx_tor = 1:nb_g_total_ligne
 
-        % actual waveguide abscisse 
+        % local waveguide abscisse 
         z_g = [0:dz:b(idx_tor)];
 
         % ALOHA mode index
@@ -91,7 +130,7 @@ for idx_pol = 1:nb_g_pol
             coeffz_wg = Phi_m(m, b(idx_tor), z_g);
 
             E_wg_m(:, :, m+1) = [coeffx_wg;coeffy_wg;coeffz_wg]*poids_E(mode_idx(m+1));
-            
+
             %% magnetic field
             coeffx_wg = zeros(size(z_g));
             coeffy_wg = -Phi_m(m, b(idx_tor), z_g);
@@ -104,21 +143,19 @@ for idx_pol = 1:nb_g_pol
 
             H_wg_m(:, :, m+1) = Ym*[coeffx_wg;coeffy_wg;coeffz_wg]*poids_H(mode_idx(m+1));
         end
-        
+        % the field in a waveguide is the sum of all modes
         E_wg = sum(E_wg_m, 3);
         H_wg = sum(H_wg_m, 3);
 
         abs_z_idx_pol = [abs_z_idx_pol, z(idx_tor) + z_g];
 
-        % field is zero on septa
+        % Set fields to zero on septa
         if (idx_tor < nb_g_total_ligne) % sauf pour le dernier
           z_e = [z(idx_tor)+b(idx_tor)+dz:dz:z(idx_tor+1)-dz];
           abs_z_idx_pol = [abs_z_idx_pol, z_e];
           E_wg = [E_wg, zeros(size(E_wg,1),length(z_e))];
           H_wg = [H_wg, zeros(size(E_wg,1),length(z_e))];  
         end
-
-        
 
         % AJOUT JH 10/2008 in order to save the results
         if (idx_tor == 1)
@@ -127,26 +164,22 @@ for idx_pol = 1:nb_g_pol
         else
             E_idx_pol = [E_idx_pol , E_wg];
             H_idx_pol = [H_idx_pol, H_wg];
-        end     
+        end
 
     end % idx_tor
-    
-
 
    % variables pour sortie & sauvegarde
-   abs_z = [];
    abs_z(idx_pol,:) = abs_z_idx_pol;
    Efield(idx_pol,:)= E_idx_pol(3,:); % z
 
    E_mouth(:,:,idx_pol) = E_idx_pol;
    H_mouth(:,:,idx_pol) = H_idx_pol; 
 
-   
    if type_swan_aloha == 1 % "a" normalization (rectangular waveguide, 3D)
     Poynting_mouth(:,:,idx_pol) = (a/2)*1/2*real(cross(E_idx_pol, conj(H_idx_pol)));
     % JH 12/09/2011
-    % I have to admit that the (a/2) term before is mainly ad-hoc, found by comparizon with
-    % COMSOL results. The a term is logical in order to transform the power density (W/m) into
+    % I have to admit that the 1/2 in the (a/2) term before is mainly ad-hoc, found by comparizon with
+    % the power balance. The a term is logical in order to transform the power density (W/m) into
     % power (W), but I have no clue for the additional /2 factor (which does not comes from 
     % the RMS definition, since there is already a 1/2 factor before the Poynting vector...).  
    elseif type_swan_aloha == 0 % no "a" dependance (parallel-plate waveguide, 2D)
@@ -156,7 +189,6 @@ for idx_pol = 1:nb_g_pol
 
    disp(aloha_message(['Summation of the poynting vector in x direction : ', num2str(Poynting_mouth_total), ' W']))
 
-    
 end % idx_pol
 
 
