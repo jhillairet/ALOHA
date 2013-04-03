a754708597d4cf528529c1e4f543b0984c540653
Julien Hillairet <julien.hillairet@cea.fr>
2013-04-03 11:59:49 +0000

    ALOHA2D FORTRAN90 version integration in ALOHA

--- libaloha/S_plasma_2D.m
+++ libaloha/S_plasma_2D.m
@@ -1,17 +1,17 @@
-% Determine le nom du binaire a utiliser
-%  
-% NB JH 01/2009 : Pour le code 2D, le nom du binaire est toujours le meme. 
-% Par contre, c'est le repertoire du binaire qui change.
-binary_name = 'couplage_plasma_2D';
-
-
+% Select the ALOHA2D binary file to use
 switch version
     case 1
+        chemin_binaire_fortran = '/code_2D/couplage_2D/ALOHA2D_F90';
+        binary_name = 'ALOHA2D.OK';
+    case 10
        chemin_binaire_fortran = '/code_2D/couplage_2D/1_couche_quad/bin';
-    case 2
+       binary_name = 'couplage_plasma_2D';
+    case 20
        chemin_binaire_fortran = '/code_2D/couplage_2D/1_couche_quad_fem/bin';
-    case 6
+       binary_name = 'couplage_plasma_2D';
+    case 60
        chemin_binaire_fortran = '/code_2D/couplage_2D/2_couches/bin';
+       binary_name = 'couplage_plasma_2D';
     otherwise 
         error('Bad defined variable : ''version''');
 end
@@ -47,13 +47,20 @@ cd([aloha_utils_getRootPath, chemin_binaire_fortran]);
    
 %  ecriture du fichier par_grill_2D.dat
 %  
-if (version == 1|2|3 ) % modeles plasma 1 couche
+if (version == 1)
+    aloha_save_ALOHA2D_inputFile('ALOHA2D.in', ...
+        'wg_nb', nbre_guides, 'wg_modes_nb', nbre_modes, 'f', freq, ...
+        'nz_min', nz_min, 'nz_max', nz_max, 'ny_min', ny_min, 'ny_max', ny_max, ...
+        'b', b_tot, 'a', a_tot, 'y', y_tot, 'z', z_tot, ...
+        'ne', ne0, 'dne', dne0, 'B0', B0);
+    
+elseif (version == 10|20 ) % modeles plasma 1 couche
     varCell = {nbre_guides nbre_modes B0 freq ne0 dne0 a_tot b_tot y_tot z_tot ny_min nz_min ny_max nz_max nbre_ny nbre_nz};
     
     aloha_saveFortranInputFile('par_grill_2D.dat', varCell);
 %      save par_grill_2D.dat  -ascii 
 
-elseif (version == 6) % modele plasma 2 couches
+elseif (version == 60) % modele plasma 2 couches
     
     save par_grill_2D.dat nbre_guides nbre_modes B0 freq ne0 [dne0 dne1] e_couche a_tot b_tot y_tot z_tot ny_min nz_min ny_max nz_max nbre_ny nbre_nz -ascii 
 
@@ -76,36 +83,52 @@ catch
     error(aloha_message('?! Binary execution problem ?!'));
 end
 
+if (version == 1)
+    % FORTRAN 90 ascii output
+    % coupling matrix
+    data=importdata('ALOHA2D.out.K.dat', ' ', 1);
 
-%  % extraction des donn�es du fichier binaire K_plasma.dat
-%  fichier_data = fopen('K_plasma.dat','r');
-%  fread(fichier_data,1,'int64'); 
-%  K_plasma_tamp = fread(fichier_data,2*nbre_modes*nbre_guides*nbre_modes*nbre_guides,'double');   
-%  K_plasma = K_plasma_tamp(1:2:2*nbre_modes*nbre_guides*nbre_modes*nbre_guides) + ...
-%                  i*K_plasma_tamp(2:2:2*nbre_modes*nbre_guides*nbre_modes*nbre_guides);
-%  K_plasma=reshape(K_plasma,nbre_modes*nbre_guides,nbre_modes*nbre_guides);
-%  fread(fichier_data,1,'double');
-%  Zhe_tamp = fread(fichier_data,2*nbre_modes*nbre_guides,'double');
-%  Zhe = Zhe_tamp(1:2:2*nbre_modes*nbre_guides) + i*Zhe_tamp(2:2:2*nbre_modes*nbre_guides);
-%  fclose(fichier_data);
+    K_cpl = complex(data.data(:,1), data.data(:,2));
+    
+    % characteristic impedances
+    data=importdata('ALOHA2D.out.Zc.dat');
+    Zhe = complex(data(:,1), data(:,2));
+    rac_Zhe = diag(sqrt(Zhe));
+    
+elseif (version == 10|20|60)
 
-%  Extraction des donnees des fichiers ascii
-[K_r, K_i]=textread('K_plasma2.dat', '(%f,%f)', 'headerlines', 2);
-[Zhe_r, Zhe_i]=textread('Zhe2.dat', '(%f,%f)', 'headerlines', 2);
+    %  % extraction des donn???es du fichier binaire K_cpl.dat
+    %  fichier_data = fopen('K_cpl.dat','r');
+    %  fread(fichier_data,1,'int64'); 
+    %  K_cpl_tamp = fread(fichier_data,2*nbre_modes*nbre_guides*nbre_modes*nbre_guides,'double');   
+    %  K_cpl = K_cpl_tamp(1:2:2*nbre_modes*nbre_guides*nbre_modes*nbre_guides) + ...
+    %                  i*K_cpl_tamp(2:2:2*nbre_modes*nbre_guides*nbre_modes*nbre_guides);
+    %  K_cpl=reshape(K_cpl,nbre_modes*nbre_guides,nbre_modes*nbre_guides);
+    %  fread(fichier_data,1,'double');
+    %  Zhe_tamp = fread(fichier_data,2*nbre_modes*nbre_guides,'double');
+    %  Zhe = Zhe_tamp(1:2:2*nbre_modes*nbre_guides) + i*Zhe_tamp(2:2:2*nbre_modes*nbre_guides);
+    %  fclose(fichier_data);
 
-Zhe = complex(Zhe_r, Zhe_i);
-rac_Zhe = diag(sqrt(Zhe));
+    %  Extraction des donnees des fichiers ascii
+    [K_r, K_i]=textread('K_cpl2.dat', '(%f,%f)', 'headerlines', 2);
+    [Zhe_r, Zhe_i]=textread('Zhe2.dat', '(%f,%f)', 'headerlines', 2);
 
-K_plasma = complex(K_r, K_i);
+    Zhe = complex(Zhe_r, Zhe_i);
+    rac_Zhe = diag(sqrt(Zhe));
 
-% on s'attend à ce que la matrice soit une matrice carree
-K_plasma=reshape(K_plasma, sqrt(length(K_plasma)), sqrt(length(K_plasma)));
+    K_cpl = complex(K_r, K_i);
 
-%  K_plasma=reshape(K_plasma,nbre_modes*nbre_guides,nbre_modes*nbre_guides);
+
+end
+
+% on s'attend ?? ce que la matrice soit une matrice carree
+K_cpl=reshape(K_cpl, sqrt(length(K_cpl)), sqrt(length(K_cpl)));
+
+%  K_cpl=reshape(K_cpl,nbre_modes*nbre_guides,nbre_modes*nbre_guides);
 
 
 %  calcul de la matrice S_plasma
-H = rac_Zhe*K_plasma*rac_Zhe;
+H = rac_Zhe*K_cpl*rac_Zhe;
 S_plasma = inv(eye(length(Zhe))+H)*(eye(length(Zhe))-H); 
   
     
