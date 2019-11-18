0f9ecc0d1d204080dcfcc6c0ab57021dd6a2fcc1
HILLAIRET Julien 218595 <JH218595@altair.partenaires.cea.fr>
2019-11-18 17:34:36 +0100

    same thing

--- tutorials/ondemand_examples/tutorial_aloha_ondemand_WEST55539.m
+++ tutorials/ondemand_examples/tutorial_aloha_ondemand_WEST55539.m
@@ -1,6 +1,6 @@
-% ALOHA tutorial : ALOHA simulation of Tore Supra shot #45525
+% ALOHA tutorial : ALOHA simulation of WEST shot #55539
 % J.Hillairet
-% October 2011
+% October 2019
 %
 %
 % In this shot, two phase shifts between modules are used in order
@@ -9,21 +9,20 @@
 % results may be used in LUKE for example.
 %
 
-% Tore Supra pulse
-TSpulse = 45524;
+%%
+% WEST pulse
+TSpulse = 55539;
 
-% Tore Supra antenna port
-% C4 - PAM launcher
-TSport = 'Q6B'; 
+% WEST antenna port
+% LH1 (FAM Antenna) : 'Q6A'
+% LH2 (PAM Antenna) : 'Q6B'
+TSport = 'Q6A'; 
 
-% -180� phasing => n//=1.7
-t_start1 = 12.3; % s 
-t_stop1  = 13.0; % s
+% start and stop times for measurement averaging
+t_start1 = 8; % s 
+t_stop1  = 9; % s
 
-% -150� phasing => n//=1.8
-t_start2 = 25; % s
-t_stop2  = 25.5; % s
-
+%%
 % First of all, before starting the ALOHA processing, we must be sure
 % that the measured data are OK. For example, sometime the phase
 % measurement can be crappy and thus leads to strange & incorrect results...
@@ -32,6 +31,7 @@ aloha_ondemand_plotPower(TSpulse, TSport, t_start1, t_stop1);
 disp('press a key to continue')
 pause
 
+%%
 % OK, the data are OK at least for the upper modules on this time interval.
 % So let's create the ALOHA scenario corresponding to this interval
 % The following function will automatically select the good antenna
@@ -43,34 +43,37 @@ pause
 % density can come from the Langmuir probes. The scrape-off gradient is
 % more delicate to determine with confidence. The following are general
 % results (2mm then 2cm) which of course can be tuned.
-sc_TS45525_12s = aloha_ondemand_scenario(TSpulse, t_start1, t_stop1, 3e17, [2e-3,2e-2], TSport);
-sc_TS45525_25s = aloha_ondemand_scenario(TSpulse, t_start2, t_stop2, 3e17, [2e-3,2e-2], TSport);
 
+% edge density [m^-3]
+ne0 = 3e17;
+% density scrape-off length [m] for the first and second plasma layers
+lambda_n = [2e-3, 2e-2];
+
+sc_WEST55538 = aloha_ondemand_scenario(TSpulse, t_start1, t_stop1, ne0, lambda_n, TSport);
+
+%%
 % here the scenarios have been generated, but not yet processed by ALOHA.
 % Let's do it !
 % (here you can go get a good tea, it is good for the health)
-sc_TS45525_12s = aloha_scenario(sc_TS45525_12s);
-sc_TS45525_25s = aloha_scenario(sc_TS45525_25s);
+sc_WEST55538 = aloha_scenario(sc_WEST55538);
 
+%%
 % save the processed results 
-aloha_scenario_save(sc_TS45525_12s, 'sc_TS45525_12s.mat');
-aloha_scenario_save(sc_TS45525_25s, 'sc_TS45525_25s.mat');
+aloha_scenario_save(sc_WEST55538, 'sc_WEST55538.mat');
 
 
-% The matlab command line display the following ALOHA results :
-% between 12.3-13s : 3.01% average power reflected ; peak n//=1.71
-% between 26.0-27s : 2.75% average power reflected ; peak n//=1.81
-
+%%
 % now we can post-process our data. First, let's have a look to the coupled
 % spectra
-aloha_plot_spectra([sc_TS45525_12s, sc_TS45525_25s]);
-    legend('TS45525 - 12s','TS45525 - 25s');
+aloha_plot_spectra([sc_WEST55538]);
+    legend('WEST 55539');
     set(gca, 'XLim', [-5, +5]);
-aloha_plot_export(gcf, 'TS45525_spectra.pdf');    % could be .png, .fig, etc.
     
-% Eventually we can export the ALOHA result to LUKE. 
+%%
+aloha_plot_export(gcf, 'WEST55539_spectra.pdf');    % could be .png, .fig, etc.
+    
+%% Eventually we can export the ALOHA result to LUKE. 
 % This is easy the following functions 
-% aloha_scenario_conversion4luke(sc_TS45525_12s, 'LUKE_TS45525_12s.mat');
-% aloha_scenario_conversion4luke(sc_TS45525_25s, 'LUKE_TS45525_25s.mat');
+aloha_scenario_conversion4luke(sc_WEST55538, 'LUKE_WEST55538.mat');
 
 
