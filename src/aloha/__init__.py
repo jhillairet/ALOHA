56216d1f58d777efe4abf4c4ffd53435801c6775
Julien Hillairet <julien.hillairet@gmail.com>
2026-03-21 17:49:58 +0100

    add waveguides and tests

---
+++ src/aloha/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "0.1.0"
+
+from . import antenna, constants, io, plasma, scenario, waveguide
+
+__all__ = [
+    "constants",
+    "waveguide",
+    "scenario",
+    "antenna",
+    "plasma",
+    "io",
+]
---
+++ src/tests/test_waveguide.py
@@ -0,0 +1,42 @@
+"""
+Testing for the Waveguide class
+"""
+import unittest
+
+import numpy as np
+from skrf import Frequency
+from skrf.media import RectangularWaveguide
+
+from aloha.waveguide import Waveguide
+
+
+class WaveguideTests(unittest.TestCase):
+
+    def test_waveguide_constructor(self):
+        wg = Waveguide(a=1e-6, b=0.5e-6)
+        assert wg.a == 1e-6
+        assert wg.b == 0.5e-6
+
+    def test_waveguide_properties(self):
+        "Test various waveguide properties for a few modes"
+        a, b = 76e-3, 14e-3
+        f0 = 3.7e9
+        wg = Waveguide(a, b)
+        # scikit-rf reference
+        freq = Frequency(f0, f0, npoints=1, unit='Hz')
+        for m in range(1, 3):
+            for n in range(0, 3):
+                for mode in ('te', 'tm'):
+                    # warning is not logged below
+                    # to hide the warnings which appears 1/0 (evanescent modes)
+                    with np.errstate(divide='ignore'):
+                        wg_skrf = RectangularWaveguide(freq, a=a, b=b, m=m, n=n, mode_type=mode)
+                        # cut-off properties
+                        assert np.isclose(wg.cutoff_wavenumber(m,n), wg_skrf.kc)
+                        assert np.isclose(wg.cutoff_frequency(m,n), wg_skrf.f_cutoff)
+                        assert np.isclose(wg.cutoff_wavelength(m,n), wg_skrf.lambda_cutoff)
+                        # guided properties
+                        assert np.isclose(wg.guided_wavenumber(f0,m,n), wg_skrf.gamma)
+                        assert np.isclose(wg.guided_wavelength(f0,m,n), wg_skrf.lambda_guide)
+                        assert np.isclose(wg.phase_velocity(f0,m,n), wg_skrf.v_p)
+                        assert np.isclose(wg.characteristic_impedance(f0,m,n,mode), wg_skrf.z0_characteristic)
