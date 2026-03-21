1f4565b135119ba28d7a8e139ae14f68243e535f
Julien Hillairet <julien.hillairet@gmail.com>
2026-03-21 20:29:39 +0100

    Added firsts draft of the antenna class

---
+++ src/aloha/antenna.py
@@ -0,0 +1,41 @@
+import numpy as np
+import tomllib
+import os
+from .constants import pi
+from .waveguide import Waveguide
+
+class Antenna(object):
+    """
+    ALOHA antenna description.
+    """
+    def __init__(self, filename: str | os.PathLike = None):
+        """
+        ALOHA antenna description.
+
+        Parameters
+        ----------
+        filename : str | os.PathLike
+            Path to a TOML ALOHA antenna file.
+
+        """
+        if filename:
+            self.antenna = self.load(filename)
+
+    @classmethod
+    def load(cls, filename: str | os.PathLike) -> dict:
+        """
+        Read an ALOHA antenna description (TOML file format).
+
+        Parameters
+        ----------
+        filename : str
+            path to a TOML file.
+
+        Returns
+        -------
+        antenna : dict
+            ALOHA antenna description.
+
+        """
+        with open(filename, "rb") as fp:
+            return tomllib.load(fp)
---
+++ src/tests/test_antenna.py
@@ -0,0 +1,25 @@
+# Antenna test
+import unittest
+from glob import glob
+from pathlib import Path
+from aloha.antenna import Antenna
+
+ANTENNAS_DIR = Path(__file__).resolve().parent.parent.parent / "antennas"
+
+class TestAntenna(unittest.TestCase):
+    def test_antenna_constructor(self):
+        filename = ANTENNAS_DIR / "simple_antenna.toml"
+        ant = Antenna(filename)
+
+    def test_validate_antenna_files(self):
+        """
+        Validation of the TOML schema of all the pre-defined antennas.
+        """
+        values = ["name", "frequency", "modules", "waveguides", "sparameters"]
+
+        antenna_filenames = glob("*.toml", root_dir=ANTENNAS_DIR)
+        for antenna_filename in antenna_filenames:
+            ant = Antenna(ANTENNAS_DIR / antenna_filename)
+            # test if the expected elements are defined
+            for value in values:
+                assert value in ant.antenna
