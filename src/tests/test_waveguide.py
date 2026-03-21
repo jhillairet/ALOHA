d1002e37e7bd7d45a2718027b9c79e3100e137ad
Julien Hillairet <julien.hillairet@gmail.com>
2026-03-21 19:28:45 +0100

    update tests

--- src/tests/test_waveguide.py
+++ src/tests/test_waveguide.py
@@ -4,11 +4,12 @@ Testing for the Waveguide class
 import unittest
 
 import numpy as np
+from scipy.signal import gammatone
 from skrf import Frequency
 from skrf.media import RectangularWaveguide
 
 from aloha.waveguide import Waveguide
-
+from aloha.constants import c, pi, Z0
 
 class WaveguideTests(unittest.TestCase):
 
@@ -41,3 +42,27 @@ class WaveguideTests(unittest.TestCase):
                         assert np.isclose(wg.phase_velocity(f0,m,n), wg_skrf.v_p)
                         assert np.isclose(wg.characteristic_impedance(f0,m,n,mode), wg_skrf.z0_characteristic)
                         assert np.isclose(wg.characteristic_admittance(f0, m, n, mode), 1/wg_skrf.z0_characteristic)
+
+    def test_characteristic_admittance(self):
+        """
+        Test characteristic admittances for TE and TM modes.
+
+        Formulas from ALOHA doc:
+        Y_c_mn = gamma_mn / (j k_0 Z_0) for TE modes
+        Y_c_mn = j k_0 / (gamma_mn Z_0) fpr TM modes
+        """
+        a, b = 76e-3, 14e-3
+        f0 = 3.7e9
+        wg = Waveguide(a, b)
+        k0 = 2*pi*f0/c
+
+        for m in range(1,3):
+            for n in range(0,3):
+                gamma_mn = wg.guided_wavenumber(f0, m, n)
+                for mode in ('te', 'tm'):
+                    if mode == 'te':
+                        Y_c_mn =  gamma_mn / (1j*k0*Z0)
+                    else:
+                        Y_c_mn = 1j*k0 / (gamma_mn*Z0)
+
+                    assert np.isclose(wg.characteristic_admittance(f0,m,n,mode), Y_c_mn)
