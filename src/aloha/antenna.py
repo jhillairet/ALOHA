0fe7f49c10838a8dab2892dae03c6bee0673ca94
Julien Hillairet <julien.hillairet@gmail.com>
2026-03-22 20:34:52 +0100

    use meaningful variable names for antenna description

--- src/aloha/antenna.py
+++ src/aloha/antenna.py
@@ -107,7 +107,7 @@ class Antenna:
         if not antenna:  # empty dict is OK
             return antenna
 
-        total_nb_modules = antenna["global"]["nma_phi"] * antenna["global"]["nma_theta"]
+        total_nb_modules = antenna["global"]["nb_mod_phi"] * antenna["global"]["nb_mod_theta"]
         # excitations
         if len(antenna["excitation"]["magnitudes"]) != total_nb_modules:
             raise ValueError(
@@ -121,9 +121,9 @@ class Antenna:
             )
 
         # module indices
-        if len(antenna["global"]["ima"]) != total_nb_modules:
+        if len(antenna["global"]["idx_mod"]) != total_nb_modules:
             raise ValueError(
-                f"Number of module indices ({len(antenna['global']['ima'])}) "
+                f"Number of module indices ({len(antenna['global']['idx_mod'])}) "
                 f"does not match total number of modules ({total_nb_modules})"
             )
 
@@ -167,29 +167,29 @@ class Antenna:
         # Extract parameters from antenna dictionary
         antenna = self.antenna
         plt.figure()
-
-        for idx_pol in range(antenna["global"]["nma_theta"]):
+        # TODO : plot all poloidal modules if more than one
+        for idx_pol in range(len(y)):
             for idx_tor in range(len(z)):
-                rect_pos = [z[idx_tor], y[idx_pol], b[idx_tor], a]
+                rect_pos = [z[idx_tor], y[idx_pol], b[idx_tor], a[idx_pol]]
                 # Create passive/active mask
-                ar_modules = np.ones(antenna["global"]["nma_phi"])
+                ar_modules = np.ones(antenna["global"]["nb_mod_phi"])
                 ar_pa_mask = np.array(antenna["module"]["mask"])
 
                 # Add passive waveguides between modules
-                ar_pa_mask = np.concatenate([ar_pa_mask, np.zeros(antenna["module"]["npwbm_phi"])])
+                ar_pa_mask = np.concatenate([ar_pa_mask, np.zeros(antenna["module"]["nb_pwg_btw_mod_phi"])])
 
                 pa_mask = np.kron(ar_modules, ar_pa_mask)
 
                 # Remove last element if there are passive waveguides between modules
-                if antenna["module"]["npwbm_phi"] > 0:
+                if antenna["module"]["nb_pwg_btw_mod_phi"] > 0:
                     pa_mask = pa_mask[:-1]
 
                 # Add passive waveguides at edges
                 pa_mask = np.concatenate(
-                    [np.zeros(antenna["module"]["npwe_phi"]), pa_mask, np.zeros(antenna["module"]["npwe_phi"])]
+                    [np.zeros(antenna["module"]["nb_pwg_edge"]), pa_mask, np.zeros(antenna["module"]["nb_pwg_edge"])]
                 )
 
-                if pa_mask[idx_tor] == 0:
+                if pa_mask[idx_tor] == 0:  # passive wg
                     plt.gca().add_patch(
                         plt.Rectangle(
                             (rect_pos[0], rect_pos[1]),
@@ -199,7 +199,7 @@ class Antenna:
                             edgecolor="k",
                         )
                     )
-                elif pa_mask[idx_tor] == 1:
+                elif pa_mask[idx_tor] == 1:  # active wg
                     plt.gca().add_patch(
                         plt.Rectangle(
                             (rect_pos[0], rect_pos[1]), rect_pos[2], rect_pos[3], fill=False, facecolor=[0.8, 0.8, 0.8]
@@ -209,16 +209,30 @@ class Antenna:
         plt.axis("equal")
         plt.xlabel("z [m]")
         plt.ylabel("y [m]")
-        # plt.title(f"Antenna architecture: {antenna['name']}\n(as view from the plasma)")
+        plt.title(f"ALOHA antenna: {antenna['name']}\n (view from the plasma)")
         plt.show()
 
     def antenna_coordinates(self):
         """
-        Extract the pertinent information for ALOHA from the antenna description.
+        Generate waveguide positions and dimensions from the antenna description.
 
         Returns
         -------
-        b, a, z, y, nwr, nwa, act_module_tor
+        b : list
+            Waveguide widths (in toroidal direction).
+        a : list
+            Waveguide heights (in poloidal direction).
+        z : list
+            Waveguide toroidal positions.
+        y : list
+            Waveguide poloidal positions.
+        nb_wg_per_row : int
+            Number of waveguides per row.
+        nb_wg_total : int
+            Total number of waveguides per antenna.
+        act_module_tor : list
+            Mask for passive/active waveguides.
+
         """
         antenna = self.antenna
         mod = antenna["global"]
@@ -226,34 +240,45 @@ class Antenna:
 
         # (total) number of waveguides per row
         # = (nb wg in a module) + 2*(nb ext wg) + (nb of wg between modules)
-        nwr = mod["nma_phi"] * wg["nwm_phi"] + 2 * wg["npwe_phi"] + (mod["nma_phi"] - 1) * wg["npwbm_phi"]
+        nb_wg_per_row = (
+            mod["nb_mod_phi"] * wg["nb_wg_phi"]
+            + 2 * wg["nb_pwg_edge"]
+            + (mod["nb_mod_phi"] - 1) * wg["nb_pwg_btw_mod_phi"]
+        )
 
         # (total) number of waveguides per column
-        nwc = mod["nma_theta"] * wg["nwm_theta"]
+        nb_wg_per_col = mod["nb_mod_theta"] * wg["nb_wg_theta"]
 
         # total number of waveguides
-        nwa = nwr * nwc
+        nb_wg_total = nb_wg_per_row * nb_wg_per_col
 
         # waveguide height - supposed constant for all the waveguides of the antenna
-        a = wg["hw_theta"]
+        a = np.full(nb_wg_total, wg["wg_size_theta"])
 
         # b
         # Make the array b which contains all the waveguide width of a row of waveguides
-        b_module = np.where(wg["mask"], wg["bwa"], wg["biwp"])
-        b_edge = np.full(wg["npwe_phi"], wg["bewp"])
-        b_inter = np.full(wg["npwbm_phi"], wg["biwp"])
+        b_module = np.where(wg["mask"], wg["awg_size_phi"], wg["pwg_size_phi"])
+        b_edge = np.full(wg["nb_pwg_edge"], wg["pwg_size_edge_phi"])
+        b_inter = np.full(wg["nb_pwg_btw_mod_phi"], wg["pwg_size_phi"])
 
-        b = np.concatenate([b_edge, np.tile(np.concatenate([b_module, b_inter]), mod["nma_phi"] - 1), b_module, b_edge])
+        b = np.concatenate(
+            [b_edge, np.tile(np.concatenate([b_module, b_inter]), mod["nb_mod_phi"] - 1), b_module, b_edge]
+        )
 
         # e
         # Make the array e which contains all the waveguide septum width of a row of waveguides
-        ne_phi = wg["npwbm_phi"] * (mod["nma_phi"] - 1) + wg["npwe_phi"] * 2 + mod["nma_phi"] * wg["nwm_phi"] - 1
-        e = np.tile(wg["biwp"], ne_phi)
+        ne_phi = (
+            wg["nb_pwg_btw_mod_phi"] * (mod["nb_mod_phi"] - 1)
+            + wg["nb_pwg_edge"] * 2
+            + mod["nb_mod_phi"] * wg["nb_wg_phi"]
+            - 1
+        )
+        e = np.tile(wg["pwg_size_phi"], ne_phi)
 
         # z
         # Make the array z which contains all the waveguide positions in the toroidal direction
-        z = np.zeros(nwr)
-        for ind in range(1, nwr):
+        z = np.zeros(nb_wg_per_row)
+        for ind in range(1, nb_wg_per_row):
             z[ind] = z[ind - 1] + b[ind - 1] + e[ind - 1]
 
         # y
@@ -261,17 +286,19 @@ class Antenna:
         h = np.concatenate(
             [
                 np.tile(
-                    np.concatenate([np.full(wg["nwm_theta"], wg["bwa"]), np.full(1, mod["sm_phi"])]),
-                    mod["nma_theta"] - 1,
+                    np.concatenate(
+                        [np.full(wg["nb_wg_theta"], wg["awg_size_phi"]), np.full(1, mod["spacing_btw_mod_phi"])]
+                    ),
+                    mod["nb_mod_theta"] - 1,
                 ),
-                np.full(wg["nwm_theta"], wg["bwa"]),
+                np.full(wg["nb_wg_theta"], wg["awg_size_phi"]),
             ]
         )
-        y = np.zeros(nwc)
-        for ind in range(1, nwc):
-            y[ind] = y[ind - 1] + h[ind - 1] + a
+        y = np.zeros(nb_wg_per_col)
+        for ind in range(1, nb_wg_per_col):
+            y[ind] = y[ind - 1] + h[ind - 1] + a[ind]
 
         # index of active waveguides in a module
-        act_module_tor = wg["mask"]  # np.where(wg['mask'] == 1)[0]
+        act_module_tor = wg["mask"]
 
-        return b, a, z, y, nwr, nwa, act_module_tor
+        return b, a, z, y, nb_wg_per_row, nb_wg_total, act_module_tor
