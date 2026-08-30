ff62779e6ccda9c17919acd9372bd293da316401
Julien Hillairet <julien.hillairet@gmail.com>
2026-08-30 10:05:39 +0200

    easier import of Scenario

--- src/aloha/__init__.py
+++ src/aloha/__init__.py
@@ -1,3 +1,4 @@
 from . import antenna, constants, plasma, utils, waveguide
+from .scenario import Scenario
 
-__all__ = ["constants", "waveguide", "antenna", "plasma", "utils"]
+__all__ = ["constants", "waveguide", "antenna", "plasma", "utils", "Scenario"]
